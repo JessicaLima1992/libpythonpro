1696ab2ab0b693e9e97c5c50c0029a3b65ba4d42
Jessica <jessica_is2@hotmail.com>
2020-05-19 15:22:54 -0300

    Parametrização de Testes

--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -1,3 +1,5 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 
 
@@ -5,12 +7,16 @@ def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
 
-def test_remetente():
+@pytest.mark.parametrize(
+    'destinatario',
+    ['foo@bar.com.br', 'jessica_is2@hotmail.com']
+)
+def test_remetente(destinatario):
     enviador = Enviador()
     resultado = enviador.enviar(
-        'jessica_is2@hotmail.com',
-        'jessica.lima6@gfatec.sp.gov.br',
+    destinatario,
+        'jessica.lima6@gfate   c.sp.gov.br',
         'Cursos Python Pro',
         'Primeira turma Luiz Vital aberta.'
     )
-    assert 'jessica_is2@hotmail.com' in resultado
+    assert destinatario in resultado
