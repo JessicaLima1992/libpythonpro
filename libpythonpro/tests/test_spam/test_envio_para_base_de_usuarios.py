8fdde7f5d5e554fae9046f5c28826f663a748d96
Jessica <jessica_is2@hotmail.com>
2020-05-21 13:49:41 -0300

    Injeção de Dependências
    E atualização do arquivo requirements-dev.txt.
    six == 1.14.0 para 1.15.0

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 
 from libpythonpro.spam.enviador_de_email import Enviador
@@ -20,14 +22,14 @@ from libpythonpro.spam.modelos import Usuario
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'jessica_is2@hotmail.com',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 class EnviadorMock(Enviador):
@@ -45,14 +47,14 @@ class EnviadorMock(Enviador):
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Jessica', email='jessica_is2@hotmail.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'rafael_is2@hotmail.com',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with(
         'rafael_is2@hotmail.com',
         'jessica_is2@hotmail.com',
         'Curso Python Pro',
